d8008441d432e2176ccbb0a46545c33d0b42445e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-05-19 21:32:24 -0400

    moving cards between sets

--- src/cljs/magewars_deckbuilder/app.cljs
+++ src/cljs/magewars_deckbuilder/app.cljs
@@ -11,12 +11,51 @@
 (def app-state
   (atom {:selected-filters (f/empty-filters f/attribute-filter-types)}))
 
+(defn remove-card
+  [data card-name src]
+  (let [count (get-in data [src :counts card-name])]
+    (if (= 1 count)
+      (assoc-in data [src :counts] (dissoc (get-in data [src :counts]) card-name))
+      (update-in data [src :counts card-name] dec))))
+
+(defn add-card
+  [data card-name dest]
+  (update-in data [dest :counts card-name] (fnil inc 0)))
+
+(defn move-card
+  [data card-name src dest]
+  (-> data
+    (remove-card card-name src)
+    (add-card card-name dest)))
+
+(defn move-card!
+  [data card-name src dest]
+  (om/transact! data #(move-card % card-name src dest)))
+
+(defn card-list [{:keys [app src dest]} owner]
+  (let [{:keys [title counts]} (src app)]
+    (reify
+      om/IRender
+      (render [_]
+        (dom/div nil
+          (dom/h2 nil title)
+          (apply
+           dom/ul nil
+           (map #(dom/li
+                     #js {:onClick
+                          (fn [e]
+                            (move-card! app (first %) src dest))}
+                     (str (first %) " " (second %)))
+                (sort-by first counts))))))))
+
 (defn app-view [app owner]
   (reify
     om/IRender
-    (render [this]
-      (apply dom/ul nil
-             (map #(dom/li nil (:name %)) (:cards app))))))
+    (render [_]
+      (dom/div nil
+        (dom/div #js {:className "cards"}
+                 (om/build card-list {:app app :src :deck :dest :pool})
+                 (om/build card-list {:app app :src :pool :dest :deck}))))))
 
 
 (edn-xhr
@@ -29,8 +68,10 @@
              (let [cindex (f/card-index cards)]
                (merge state {:cards cards
                              :cindex cindex
-                             :deck-count {}
-                             :pool-count (into {} (map (juxt :name :count) cards))})))
+                             :deck {:title "Deck"
+                                    :counts {}}
+                             :pool {:title "Pool"
+                                    :counts (into {} (map (juxt :name :count) cards))}})))
            cards)
     (om/root app-view app-state
              {:target (.getElementById js/document "app")}))})
