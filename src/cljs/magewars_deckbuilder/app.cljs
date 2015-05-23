76d3f7d9e98118b591cb3a8f3e432b8ceab5d409
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-05-23 11:09:48 -0400

    showing card details

--- src/cljs/magewars_deckbuilder/app.cljs
+++ src/cljs/magewars_deckbuilder/app.cljs
@@ -13,14 +13,45 @@
 (def app-state
   (atom {:selected-filters (f/empty-filters f/attribute-filter-types)}))
 
+(defn row
+  [x y]
+  (dom/tr nil
+    (dom/td nil x)
+    (dom/td nil y)))
+
+(defn display-many
+  [x]
+  (->> x
+       sort
+       (map name)
+       (clojure.string/join ", ")))
+
+(defn card-details
+  [c]
+  (dom/table nil
+    (row "Type" (name (:type c)))
+    (row "Subtypes" (display-many (:subtypes c)))
+    (row "School" (name (first (:school c))))
+    (row "Level" (second (:school c)))
+    (row "Casting Cost" (:casting-cost c))
+    (row "Speed" (name (:speed c)))
+    (row "Targets" (display-many (:targets c)))))
+
 (defn selected-card-view [{:keys [selected-card cards-by-name]} owner]
   (reify
     om/IRender
     (render [_]
-      (let [card (get cards-by-name selected-card)]
+      (if-let [card (get cards-by-name selected-card)]
         (dom/div nil
-          (if card
-            (dom/h3 nil (:name card))))))))
+          (dom/h3 nil (:name card))
+          (dom/img #js {:src (str "images/" (get-in card [:display :image]))})
+          (dom/div #js {:className "card-details"}
+                   (card-details (:display card))))
+        (dom/div nil)))))
+
+
+
+
 
 (edn-xhr
  {:method :get
