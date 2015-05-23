3f98b55bb2d9df3919195d0da8ec75d7cba84bb0
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-05-23 10:28:25 -0400

    images

---
+++ resources/data/cards/image-num.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+index = 1
+
+sets = {"attacks" => "MW1A"}
+
+
+sets.each {|k, v|
+  text = File.read("#{k}.edn")
+  new_text = ""
+  text.each_line {|line|
+    if line =~ /:description/
+      num = sprintf("%02d", index);
+      index += 1;
+      new_text += line.sub(Regexp.new('(:description .*")}'), "\\1\n   :image \"#{v}#{num}.jpg\"}")
+    else
+      new_text += line
+    end
+  }
+  puts new_text
+}
