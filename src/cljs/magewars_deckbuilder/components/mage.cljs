79c9d5c10f9bf192424c45ce7f58f786b3ee8888
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-06-05 12:51:19 -0400

    working on getting selected element working

--- src/cljs/magewars_deckbuilder/components/mage.cljs
+++ src/cljs/magewars_deckbuilder/components/mage.cljs
@@ -38,32 +38,41 @@
 (defn wizard? [x] (= "Wizard" (:class x)))
 
 (defn select-mage
-  [mage select-mage]
-  (om/update! select-mage mage))
+  [{:keys [mage selected-mage selected-element]}]
+  (om/update! selected-mage mage)
+  (when-not (wizard? mage)
+    (om/update! selected-element {})))
 
-(defn mage-li [{:keys [mage selected-mage]} owner]
+(defn mage-li [data owner]
   (reify
     om/IRender
     (render [_]
       (dom/li nil
         (dom/label nil
           (dom/input #js {:type "radio" :name "mage"
-                          :onClick #(om/update! selected-mage mage)})
-          (:class mage))))))
+                          :onClick #(select-mage data)})
+          (get-in data [:mage :class]))))))
 
 (defn element-li
-  [element selected-element]
-  (dom/li nil
-    (dom/label nil
-      (dom/input #js {:type "radio" :name "element"})
-      (name element))))
+  [{:keys [element selected-element]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil
+        (dom/label nil
+          (dom/input #js {:type "radio" :name "element"
+                          :onChange #(om/update! selected-element element)})
+          (name element))))))
 
 (defn wizard-element [{:keys [selected-mage selected-element]} owner]
   (reify
     om/IRender
     (render [_]
       (apply dom/ul #js {:className (if-not (= "Wizard" (:class selected-mage)) "hidden")}
-             (map element-li elements (repeat selected-element))))))
+             (om/build-all element-li (map (fn [e se] {:element e
+                                                      :selected-element se})
+                                           elements
+                                           (repeat selected-element)))))))
 
 (defn mage-stats [{:keys [selected-element selected-mage]} owner]
   (reify om/IRender
