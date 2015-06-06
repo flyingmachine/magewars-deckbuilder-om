1a1402973ed12a880e29eb8eae1becc518ea3d2d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-06-06 15:02:27 -0400

    pull app data from build cljs file

--- src/cljs/magewars_deckbuilder/app.cljs
+++ src/cljs/magewars_deckbuilder/app.cljs
@@ -5,6 +5,7 @@
             [om-sync.util :refer [tx-tag edn-xhr]]
             [cljs.core.async :as async :refer [chan]]
             [magewars-deckbuilder.filtering :as f]
+            [magewars-deckbuilder.data.all :refer [data]]
             [magewars-deckbuilder.components.card-list :as cl]
             [magewars-deckbuilder.components.filter-selection :as fs]
             [magewars-deckbuilder.components.selected-card :as sc]
@@ -13,34 +14,26 @@
 (enable-console-print!)
 
 (def app-state
-  (atom {:selected-filters (f/empty-filters f/attribute-filter-types)
-         :mage-selection {:selected-mage nil
-                          :selected-element nil}}))
+  (let [{:keys [cards mages]} data
+        cindex (f/card-index cards)]
+    (atom {:selected-filters (f/empty-filters f/attribute-filter-types)
+           :mage-selection {:selected-mage nil
+                            :selected-element nil}
+           :cards cards
+           :cards-by-name (into {} (map (juxt :name identity) cards))
+           :cindex cindex
+           :deck {:title "Deck"
+                  :counts {}}
+           :pool {:title "Pool"
+                  :counts (into {} (map (juxt :name :count) cards))}
+           :mages mages})))
 
-(edn-xhr
- {:method :get
-  :url "/data"
-  :on-complete
-  (fn [cards]
-    (swap! app-state
-           (fn [state {:keys [cards mages]}]
-             (let [cindex (f/card-index cards)]
-               (merge state {:cards cards
-                             :cards-by-name (into {} (map (juxt :name identity) cards))
-                             :cindex cindex
-                             :deck {:title "Deck"
-                                    :counts {}}
-                             :pool {:title "Pool"
-                                    :counts (into {} (map (juxt :name :count) cards))}
-                             :mages mages})))
-           cards)
-
-    (om/root fs/filter-list app-state
-             {:target (.getElementById js/document "filters")
-              :shared {:toggle-filter (chan)}})
-    (om/root m/mage-view app-state
-             {:target (.getElementById js/document "mage")})
-    (om/root cl/all-cards-view app-state
-             {:target (.getElementById js/document "cards")})
-    (om/root sc/selected-card-view app-state
-             {:target (.getElementById js/document "selected-card")}))})
+(om/root fs/filter-list app-state
+         {:target (.getElementById js/document "filters")
+          :shared {:toggle-filter (chan)}})
+(om/root m/mage-view app-state
+         {:target (.getElementById js/document "mage")})
+(om/root cl/all-cards-view app-state
+         {:target (.getElementById js/document "cards")})
+(om/root sc/selected-card-view app-state
+         {:target (.getElementById js/document "selected-card")})
